613ef710505b438963fe3f6c57da16f70b9a3ae3
sapna931 <sapnarathi319@gmail.com>
2026-06-01 23:17:51 +0530

    fixed repository scanning

--- src/main/java/com/hospital/backend/BackendApplication.java
+++ src/main/java/com/hospital/backend/BackendApplication.java
@@ -2,10 +2,11 @@ package com.hospital.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.hospital.backend.repository")
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
--- src/main/java/com/hospital/backend/repository/PatientRepository.java
+++ src/main/java/com/hospital/backend/repository/PatientRepository.java
@@ -2,6 +2,8 @@ package com.hospital.backend.repository;
 
 import com.hospital.backend.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface PatientRepository extends JpaRepository<Patient, Integer> {
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
